ab6dee0e04af9f6882513ebd268b7851cd43d371
Wooya <wooya_0@naver.com>
2024-09-27 00:26:00 +0900

    메인화면 비율 조정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,33 +1,23 @@
-import {Routes, Route, BrowserRouter } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import AdminLogin from './admin/js/AdminLogin.js';
 import AdminMain from './admin/js/AdminMain.js';
 import MemInfo from './admin/js/MemInfo.js';
-import Main from './Main.js';
+import Main from './global/Main.js';
 import LoginMain from "./user/js/LoginMain";
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <main>
       <Routes>
-        <Route path="/" element={ <>
-                            <Main />
-                        </>} />
+        <Route path="/" element={ <><Main /></>} />
 
         {/* 어드민 페이지 */}
-        <Route path="/admin/login" 
-          element={<>
-            <AdminLogin />
-          </>} />
+        <Route path="/admin/login" element={<><AdminLogin /></>} />
         <Route 
-          path="/admin" 
-          element={<>
-            <AdminMain />
-          </>} />
+          path="/admin" element={<><AdminMain /></>} />
           <Route 
-          path="/admin/meminfo" 
-          element={<>
-            <MemInfo />
-          </>} />
+          path="/admin/meminfo" element={<><MemInfo /></>} />
 
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminMain />} />
@@ -35,7 +25,7 @@ function App() {
       {/* Page Login */}
           <Route path="/user/js/LoginMain" element={<LoginMain/>}/>
       </Routes>
-    </BrowserRouter>
+      </main>
   )
 }
 
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+import Header from "./global/Header.js";
+import Footer from "./global/Footer.js";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Header />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Footer />
+  </BrowserRouter>
 );
