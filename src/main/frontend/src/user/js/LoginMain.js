01c9921f83f2e9c1b50c06032a9f8971ec189df1
Wooya <wooya_0@naver.com>
2024-09-27 00:42:28 +0900

    병합

---
+++ src/main/frontend/src/user/js/LoginMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class LoginMain extends React.Component {
+    render() {
+        return (
+                <h1>test</h1>
+        )
+    }
+}
+
+export default LoginMain;
