b5f0e6bb824e35f44855b2c1c8832c7eb8e5ff81
Jin <wlsdyd0824@gmail.com>
2024-09-26 16:37:51 +0900

    test02

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,33 +3,23 @@ import AdminLogin from './admin/js/AdminLogin.js';
 import AdminMain from './admin/js/AdminMain.js';
 import MemInfo from './admin/js/MemInfo.js';
 import Main from './Main.js';
+import LoginMain from "./user/js/LoginMain";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <>
-                            <Main />
-                        </>} />
+        <Route path="/" element={ <><Main /></>} />
 
         {/* 어드민 페이지 */}
-        <Route path="/admin/login" 
-          element={<>
-            <AdminLogin />
-          </>} />
-        <Route 
-          path="/admin" 
-          element={<>
-            <AdminMain />
-          </>} />
-          <Route 
-          path="/admin/meminfo" 
-          element={<>
-            <MemInfo />
-          </>} />
-
+        <Route path="/admin/login" element={<><AdminLogin /></>} />
+        <Route path="/admin" element={<><AdminMain /></>} />
+        <Route path="/admin/meminfo" element={<><MemInfo /></>} />
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminMain />} />
+
+      {/* 로그인 페이지 */}
+          <Route path={"/login"} element={<LoginMain/>}/>
       </Routes>
     </BrowserRouter>
   )
