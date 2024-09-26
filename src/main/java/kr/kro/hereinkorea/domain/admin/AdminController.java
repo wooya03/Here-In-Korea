8bc8dc4385f8b0e80110a90d9a04fb204ac9a113
Wooya <wooya_0@naver.com>
2024-09-26 10:35:11 +0900

    admin Controller

---
+++ src/main/java/kr/kro/hereinkorea/domain/admin/AdminController.java
@@ -0,0 +1,10 @@
+package kr.kro.hereinkorea.domain.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @GetMapping("/admin")
+    public
+}
