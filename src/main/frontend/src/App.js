735f86d705a903d68c9741d6f2bff75b7ea43757
Wooya <wooya_0@naver.com>
2024-09-26 15:39:14 +0900

    어드민 내용 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import AdminLogin from './admin/js/AdminLogin.js';
 import AdminMain from './admin/js/AdminMain.js';
 import Main from './Main.js';
-import LoginMain from "./user/js/LoginMain";
 
 function App() {
   return (
@@ -14,9 +13,6 @@ function App() {
         {/* 어드민 페이지 */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminMain />} />
-
-      {/* Page Login */}
-          <Route path="/user/js/LoginMain" element={<LoginMain/>}/>
       </Routes>
     </BrowserRouter>
   )
