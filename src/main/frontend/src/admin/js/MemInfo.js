1a1ab37315a078ecb9c832ee454e881096f5f1ae
Wooya <wooya_0@naver.com>
2024-09-26 15:39:14 +0900

    어드민 내용 추가

---
+++ src/main/frontend/src/admin/js/MemInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MemInfo extends React.Component {
+    render() {
+        return (
+            <div className="">
+                회원정보조회 페이지
+            </div>
+        )
+    }
+}  
+
+export default MemInfo;
