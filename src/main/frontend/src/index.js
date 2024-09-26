b7bd8112a0626fee448b2ccd80786e858b7a74c2
Wooya <wooya_0@naver.com>
2024-09-27 01:42:48 +0900

    라우터 수정

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import Header from "./global/Header.js";
-import Footer from "./global/Footer.js";
+import Footer from "./global/footer/Footer.js";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Header />
     <App />
     <Footer />
   </BrowserRouter>
