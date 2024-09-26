6e479e135aa24b9f3027bf835bcdbe56eadc1181
Jin <wlsdyd0824@gmail.com>
2024-09-26 15:43:30 +0900

    make Loginpage

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AdminLogin from './admin/js/AdminLogin.js';
 import AdminMain from './admin/js/AdminMain.js';
 import Main from './Main.js';
+import LoginMain from "./user/js/LoginMain";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         {/* 어드민 페이지 */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminMain />} />
+
+      {/* Page Login */}
+          <Route path="/user/js/LoginMain" element={<LoginMain/>}/>
       </Routes>
     </BrowserRouter>
   )
