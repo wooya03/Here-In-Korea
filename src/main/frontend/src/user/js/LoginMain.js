f4a86ddc85688f9976668b899494399447df09fe
Jin <wlsdyd0824@gmail.com>
2024-09-27 13:03:22 +0900

    로그인 페이지 생성

--- src/main/frontend/src/user/js/LoginMain.js
+++ src/main/frontend/src/user/js/LoginMain.js
@@ -3,9 +3,17 @@ import React from "react";
 class LoginMain extends React.Component {
     render() {
         return (
-            <div>
-                <br/>
-                <h1>testing</h1>
+            <div className="content">
+                <div className="login_wrap">
+                    <ul className="menu_wrap"></ul>
+                    <form className="login_form" id="login_form">
+                        <ul className="panel_wrap">
+                            <li className="panel_item"></li>
+                        </ul>
+                    </form>
+
+                </div>
+
             </div>
 
 
