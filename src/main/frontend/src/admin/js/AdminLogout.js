236d2dead97a1d6090bcf7753416d0cae50a3d95
Wooya <wooya_0@naver.com>
2024-10-04 11:14:18 +0900

    로그아웃

---
+++ src/main/frontend/src/admin/js/AdminLogout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class AdminLogout extends React.Component {
+    render(){
+        return (
+            <h1>로그아웃</h1>
+        )
+    }
+}
+
+export default AdminLogout;
