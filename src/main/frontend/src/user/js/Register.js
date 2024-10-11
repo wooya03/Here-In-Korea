df7da8a1455c06bd3cb419d97ca0881c2c8e7850
Jin <wlsdyd0824@gmail.com>
2024-10-11 13:50:06 +0900

    make register page

---
+++ src/main/frontend/src/user/js/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Register = () =>{
+
+    return(
+        <div className="content">
+            <div className="join_wrap">
+                
+            </div>
+
+        </div>
+    )
+}
+
