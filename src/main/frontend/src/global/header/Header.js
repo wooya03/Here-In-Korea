4a704b3d813154d4e019c01f930a3055633ca806
Jin <wlsdyd0824@gmail.com>
2024-10-31 10:56:52 +0900

    로그인 버튼 클릭시 로그인 페이지 이동 (임시)

--- src/main/frontend/src/global/header/Header.js
+++ src/main/frontend/src/global/header/Header.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Link 추가
 import "./Header.css";
+import {useNavigate} from "react-router-dom";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
   const [guestImage, setGuestImage] = useState(`${process.env.PUBLIC_URL}/Image/guest1.png`); // 초기 이미지를 guest1.png로 설정
+  const navigate = useNavigate(); // 라우터 네비게이트 이동을 위한 생성
 
   const handleLoginLogout = () => {
     if (isLoggedIn) {
       setGuestImage(`${process.env.PUBLIC_URL}/Image/guest1.png`); // 게스트 이미지로 변경
       setIsLoggedIn(false); // 로그인 상태를 false로 변경
+      navigate("/");  //로그아웃이후 메인페이지로 이동
+
     } else {
       setGuestImage(`${process.env.PUBLIC_URL}/Image/user1.png`); // 사용자 이미지로 변경
       setIsLoggedIn(true); // 로그인 상태를 true로 변경
+      navigate("/loginMain"); //로그인 페이지로 이동
     }
   };
 
