9ea860912cdc2305099e3ced71e2e72fd98d642f
AirtoFresh <airtofresh@gmail.com>
2024-11-06 11:09:24 +0900

    헤더CSS 수정

--- src/main/java/kr/kro/hereinkorea/HereinkoreaApplication.java
+++ src/main/java/kr/kro/hereinkorea/HereinkoreaApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HereinkoreaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HereinkoreaApplication.class, args);
+
+
 	}
 
 }
