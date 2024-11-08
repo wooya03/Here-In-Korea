93cf95ddd484db335c4f1f06266ac6636a2ce5d0
Jin <wlsdyd0824@gmail.com>
2024-11-08 14:58:26 +0900

    회원가입 css 수정

--- src/main/frontend/src/user/js/RegisterAuth.js
+++ src/main/frontend/src/user/js/RegisterAuth.js
@@ -13,13 +13,13 @@ const RegisterAuth = () =>{
     return(
         <div className="content">
             <div className="form_wrap">
-                    <div className="form_item">
+                    <div className="auth_form">
                         <p>인증번호 입력</p>
                         <input type="text" className="auth" name="auth" maxLength="16"/>
                     </div>
 
-                <div className="register_btn">
-                    <button className="auth_request_btn" onClick={handleRegister}>인증 요청</button>
+                <div className="register_btn_form">
+                    <button className="register_btn" onClick={handleRegister}>인증 요청</button>
                 </div>
             </div>
         </div>
