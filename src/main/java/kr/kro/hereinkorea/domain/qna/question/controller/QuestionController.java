6858a1e453a2ba5ad3f5ecdb676d66f087ee233d
Wooya <wooya_0@naver.com>
2024-11-15 09:08:00 +0900

    Question 매핑명 수정

--- src/main/java/kr/kro/hereinkorea/domain/qna/question/controller/QuestionController.java
+++ src/main/java/kr/kro/hereinkorea/domain/qna/question/controller/QuestionController.java
@@ -1,15 +1,19 @@
 package kr.kro.hereinkorea.domain.qna.question.controller;
 
 import kr.kro.hereinkorea.domain.qna.question.dto.QuestionDTO;
+import kr.kro.hereinkorea.domain.qna.question.repository.QuestionRepository;
+import kr.kro.hereinkorea.domain.qna.question.service.QuestionService;
 import kr.kro.hereinkorea.global.common.dto.PageRequestDTO;
 import kr.kro.hereinkorea.global.common.dto.PageResultDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/qna")
+@RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
+
+    private final QuestionService questionService;
 
     @GetMapping("/list")
     public PageResultDTO<QuestionDTO, Object[]> list(PageRequestDTO requestDTO){
