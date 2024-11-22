8018c40205e9d8ced24c5236dd6d7fb9986e8f1b
Wooya <wooya_0@naver.com>
2024-11-22 14:27:57 +0900

    상세 페이지 쿼리

--- src/main/java/kr/kro/hereinkorea/domain/qna/question/repository/QuestionRepository.java
+++ src/main/java/kr/kro/hereinkorea/domain/qna/question/repository/QuestionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
 
@@ -17,5 +18,13 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> 
                     "FROM QuestionEntity q ")
     Page<Object[]> getQuestionCount(Pageable pageable);
 
+
+    @Query("SELECT q, m, a " +
+            "FROM QuestionEntity q " +
+            "LEFT JOIN q.member m " +
+            "LEFT JOIN q.id = a.questionId " +
+            "WHERE q.qId = :id " +
+            "GROUP BY q ")
+    Object getQuestionById(@Param("id") Long id);
 
 }
