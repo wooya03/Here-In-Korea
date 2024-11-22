290c566c9f9b4df5c0f64408a720ae265650637d
Wooya <wooya_0@naver.com>
2024-11-22 14:32:57 +0900

    QuestionServiceTest

--- src/test/java/kr/kro/hereinkorea/domain/qna/question/service/QuestionServiceTest.java
+++ src/test/java/kr/kro/hereinkorea/domain/qna/question/service/QuestionServiceTest.java
@@ -7,10 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Random;
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class QuestionServiceTest {
