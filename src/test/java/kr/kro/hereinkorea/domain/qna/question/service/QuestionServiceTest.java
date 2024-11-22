e7bc58a428cf837cd51fddf1e56694216171b545
Wooya <wooya_0@naver.com>
2024-11-22 09:18:45 +0900

    Question Insert Test

--- src/test/java/kr/kro/hereinkorea/domain/qna/question/service/QuestionServiceTest.java
+++ src/test/java/kr/kro/hereinkorea/domain/qna/question/service/QuestionServiceTest.java
@@ -1,6 +1,8 @@
 package kr.kro.hereinkorea.domain.qna.question.service;
 
+import kr.kro.hereinkorea.domain.member.Entity.MemberEntity;
 import kr.kro.hereinkorea.domain.qna.question.entity.QuestionEntity;
+import kr.kro.hereinkorea.domain.qna.question.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class QuestionServiceTest {
     @Autowired
-    private QuestionService questionService;
+    private QuestionRepository questionRepository;
+    @Test
+    void testInsert() {
+        MemberEntity memberEntity = MemberEntity.builder().memId("user001").build();
 
+
+        QuestionEntity questionEntity = QuestionEntity.builder()
+                .qCategory("기타문의")
+                .qTitle("test1")
+                .qStatus(true)
+                .qContents("test111")
+                .member(memberEntity)
+                .build();
+
+        questionRepository.save(questionEntity);
+    }
 }
