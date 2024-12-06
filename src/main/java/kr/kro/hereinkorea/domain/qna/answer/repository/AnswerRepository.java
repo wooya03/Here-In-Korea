90e0d955b6b2549553612035bdf098cbb5c38e22
Wooya <wooya_0@naver.com>
2024-12-06 14:32:19 +0900

    임시

--- src/main/java/kr/kro/hereinkorea/domain/qna/answer/repository/AnswerRepository.java
+++ src/main/java/kr/kro/hereinkorea/domain/qna/answer/repository/AnswerRepository.java
@@ -2,13 +2,11 @@ package kr.kro.hereinkorea.domain.qna.answer.repository;
 
 import kr.kro.hereinkorea.domain.qna.answer.entity.AnswerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
 
-   Optional<AnswerEntity> findByQuestionId(long id);
+//   Optional<AnswerEntity> findByQuestionId(long id);
 
 }
