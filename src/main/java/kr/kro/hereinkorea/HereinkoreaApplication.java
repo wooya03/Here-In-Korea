cd837aed5bb4348a517c8aa6f5d97ede90afd762
Wooya <wooya_0@naver.com>
2024-12-10 07:15:31 +0900

    restTemplate를 JacksonConfig로 이동

--- src/main/java/kr/kro/hereinkorea/HereinkoreaApplication.java
+++ src/main/java/kr/kro/hereinkorea/HereinkoreaApplication.java
@@ -2,9 +2,7 @@ package kr.kro.hereinkorea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
