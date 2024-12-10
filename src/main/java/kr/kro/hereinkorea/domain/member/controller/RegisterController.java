9da47a7b9db3adbea8cdcb6005d4496745146664
Jin <wlsdyd0824@gmail.com>
2024-12-10 09:39:52 +0900

    CrossOrigin 도메인 다른점 임시로 맞춘 상태

--- src/main/java/kr/kro/hereinkorea/domain/member/controller/RegisterController.java
+++ src/main/java/kr/kro/hereinkorea/domain/member/controller/RegisterController.java
@@ -7,14 +7,12 @@ import kr.kro.hereinkorea.domain.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 //경로 수정 예정
+@CrossOrigin(origins = "http://localhost:3001", allowCredentials = "true")
 public class RegisterController {
     @Autowired
     private  MemberService memberService;
