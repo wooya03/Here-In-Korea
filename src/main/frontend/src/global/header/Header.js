965b6e9627686014e796f675226b3708a7bc96d4
Jin <wlsdyd0824@gmail.com>
2024-12-10 09:37:54 +0900

    경로 수정 및 주석 추가

--- src/main/frontend/src/global/header/Header.js
+++ src/main/frontend/src/global/header/Header.js
@@ -16,7 +16,7 @@ function Header() {
     } else {
       setGuestImage(`${process.env.PUBLIC_URL}/Image/user1.png`);
       setIsLoggedIn(true);
-      navigate("/login");
+      navigate("/user/login");
     }
   };
 
@@ -48,7 +48,7 @@ function Header() {
         <nav className="nav">
           <ul className="menu">
             <li className="menu-item first-item"><Link to="/">메인</Link></li>
-            <li className="menu-item"><Link to="/festival">행사</Link></li>
+            <li className="menu-item"><Link to="/event">행사</Link></li>
             <li className="menu-item"><Link to="/review">리뷰</Link></li>
             <li className="menu-item"><Link to="/course">여행코스</Link></li>
             <li className="menu-item"><Link to="/accommodation">숙박</Link></li>
