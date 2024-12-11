5b1bc351b40a93c0cc90ffa5b4cd96130843deaf
Wooya <wooya_0@naver.com>
2024-12-12 00:18:14 +0900

    .

--- src/main/java/kr/kro/hereinkorea/domain/event/repository/EventImgRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.kro.hereinkorea.domain.event.repository;
-
-import kr.kro.hereinkorea.domain.event.entity.EventImgEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventImgRepository extends JpaRepository<EventImgEntity, Long> {
-}
