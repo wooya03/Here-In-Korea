a3d45060c79a0a6aec3caeed4337aa005db5337d
Wooya <wooya_0@naver.com>
2024-12-15 16:56:00 +0900

    답변 등록

--- src/main/java/kr/kro/hereinkorea/domain/qna/answer/controller/AnswerController.java
+++ src/main/java/kr/kro/hereinkorea/domain/qna/answer/controller/AnswerController.java
@@ -11,17 +11,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AnswerController {
 
-//    private final AnswerService answerService;
-//
-//    @PostMapping("/write")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public String addAnswer(@RequestBody AnswerDTO answerDTO) {
-//        try {
-//            answerService.write(answerDTO);
-//            return "답변이 등록되었습니다.";
-//        } catch (Exception e) {
-//            e.printStackTrace();  // 로그에 에러 출력
-//            return "문제 발생: " + e.getMessage();
-//        }
-//    }
+    private final AnswerService answerService;
+
+    @PostMapping("/write")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addAnswer(@RequestBody AnswerDTO answerDTO) {
+        try {
+            answerService.write(answerDTO);
+            return "답변이 등록되었습니다.";
+        } catch (Exception e) {
+            e.printStackTrace();  // 로그에 에러 출력
+            return "문제 발생: " + e.getMessage();
+        }
+    }
 }
