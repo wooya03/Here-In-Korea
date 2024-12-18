a1a6cd15ca59e691750391d8138dcc98fd9b0ca3
Wooya <wooya_0@naver.com>
2024-12-19 03:06:38 +0900

    문의 삭제 추가

--- src/main/java/kr/kro/hereinkorea/domain/admin/service/AdminQuestionService.java
+++ src/main/java/kr/kro/hereinkorea/domain/admin/service/AdminQuestionService.java
@@ -1,7 +1,9 @@
 package kr.kro.hereinkorea.domain.admin.service;
 
 import kr.kro.hereinkorea.domain.member.Entity.MemberEntity;
+import kr.kro.hereinkorea.domain.qna.answer.dto.AnswerDTO;
 import kr.kro.hereinkorea.domain.qna.answer.entity.AnswerEntity;
+import kr.kro.hereinkorea.domain.qna.answer.repository.AnswerRepository;
 import kr.kro.hereinkorea.domain.qna.question.dto.QuestionDTO;
 import kr.kro.hereinkorea.domain.qna.question.entity.QuestionEntity;
 import kr.kro.hereinkorea.domain.qna.question.repository.QuestionRepository;
@@ -13,10 +15,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AdminQuestionService {
     @Autowired
     QuestionRepository questionRepository;
+    @Autowired
+    AnswerRepository answerRepository;
 
     public PageResultDTO<QuestionDTO, Object[]> getQuestion(String title, String category, PageRequestDTO pageRequestDTO) {
         Pageable pageable = pageRequestDTO.getPageable(Sort.by("id").descending());
@@ -52,7 +58,21 @@ public class AdminQuestionService {
                 .memName(user.getMemName())
                 .createdDate(entity.getCreatedDate())
                 .modifiedDate(entity.getModifiedDate())
-                .answerContents(answer != null ? answer.getContents() : null) // null 체크 추가
+                .answerContents(answer != null ? answer.getContents() : null)
                 .build();
+    }
+
+    public void deleteQuestion(List<Long> questionIds) {
+        try {
+            for(Long id : questionIds){
+                if(!answerRepository.findByQuestionId(id).isEmpty()){
+                    Long aid = AnswerDTO.builder().questionId(id).build().getId();
+                    answerRepository.deleteById(aid);
+                }
+                questionRepository.deleteById(id);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("문의 삭제 중 오류 발생", e);
+        }
     }
 }
