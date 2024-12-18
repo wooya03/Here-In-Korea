d78260f58458f91e246490cbe2f71b261e1f71fa
Wooya <wooya_0@naver.com>
2024-12-19 03:07:35 +0900

    로그아웃

--- src/main/frontend/src/admin/js/AdminLogout.js
+++ src/main/frontend/src/admin/js/AdminLogout.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
 const AdminLogout = () => {
     const token = localStorage.getItem("token");
