9073aaa62a4bd60fd3d06fab498953637e826e7a
hwang <hwangyujin22@gmail.com>
2024-12-19 20:35:39 +0900

    코스 파일 날아간거 복구중

--- src/main/frontend/src/course/js/Course.js
+++ src/main/frontend/src/course/js/Course.js
@@ -3,6 +3,7 @@ import Header from "../../global/header/Header";
 import "../css/Course.css";
 import {Link, useNavigate} from "react-router-dom";
 import { useAuth } from "../../global/auth_context/AuthContext";
+import axios from "axios";
 
 function Course() {
   const { isLoggedIn } = useAuth(); //로그인 상태 확인
@@ -10,10 +11,44 @@ function Course() {
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const coursesPerPage = 6;
+  const [totalPages, setTotalPages] = useState(0);  // 총 페이지 수 상태
   const navigate = useNavigate(); //Link 보다 이거 이용하는게 편할 거에요
 
   useEffect(() => {
-    fetchCourses();
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/course", {
+          params: {
+            page: currentPage - 1,   // 페이지 번호는 0부터 시작
+            size: coursesPerPage,    // 페이지 크기
+            sortBy: 'createdDate'
+          }
+        });
+        console.log(response.data);  // 데이터를 확인하기 위해 콘솔 로그 추가
+    
+        const transformedData = response.data.dtoList 
+          ? response.data.dtoList.map((item) => ({
+              courseId: item.courseId,
+              courseTitle: item.courseTitle,
+              courseContent: item.courseContent,
+              courseTag: item.courseTag,
+              createdDate: new Date(item.createdDate).toLocaleString(), // 날짜 변환
+              courseViews: item.courseViews,
+              courseLikes: item.courseLikes,
+              courseName: item.courseName,
+              images: item.images, // CourseImageDTO에 해당하는 이미지 리스트
+          }))
+          : [];
+    
+        setCourseData(transformedData); // 상태 업데이트
+        setTotalPages(response.data.totalPages); // totalPages 값 설정
+        setLoading(false);
+      } catch (error) {
+        console.error("코스 데이터 가져오기 실패:", error);
+        setLoading(false);
+      }
+    };
+    
   }, [currentPage]); // currentPage가 변경될 때마다 fetchCourses 함수가 호출되도록 설정
 
   // 페이지 변경 함수
