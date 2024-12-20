64704113977b6a2ed97dd076b20b3ad921846cf7
Wooya <wooya_0@naver.com>
2024-12-20 14:55:28 +0900

    로그아웃 처리

--- src/main/frontend/src/admin/js/AdminHeader.js
+++ src/main/frontend/src/admin/js/AdminHeader.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import '../css/AdminHeader.css';
 import image from '../../img/logo.png';
+import { useAuth } from "../../global/auth_context/AuthContext";
 
-class AdminHeader extends React.Component {
-    render (){
+const AdminHeader = () => {
+  const { isLoggedIn, checkLoginStatus } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+    alert("로그아웃 되었습니다!!!");
+    checkLoginStatus();
+    navigate(0);
+  };
+
         return (
             <header className="header">
       <div className="logoimage">
@@ -19,14 +29,13 @@ class AdminHeader extends React.Component {
               <li className="menu-item"><Link to="/admin/course">코스게시판관리</Link></li>
               <li className="menu-item"><Link to="/admin/review">리뷰게시판관리</Link></li>
               <li className="menu-item"><Link to="/admin/qna">Q&A</Link></li>
-              <li className="menu-item last-item"><Link to="/admin/logout">LOGOUT</Link></li>
+              <li className="menu-item last-item"><Link onClick={handleLogout}>LOGOUT</Link></li>
             </ul>
           </nav>
         </div>
       </div>
     </header>
-        )
-    }
+  )
 }
 
 
